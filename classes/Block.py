345b6beb5d25b96ebc3d675f335fc2538086cedc
adbaga <bagaskaraiqbal7a@gmail.com>
2022-02-12 15:59:22 +0200

    block visualization

--- classes/Block.py
+++ classes/Block.py
@@ -1,7 +1,13 @@
 class Block:
-    def __init__(self, b_name, ep_E1, ep_E2):  
-        self.b_name = b_name
-        self.ib1 = ep_E1
-        self.ib2 = ep_E2
-        self.b_cardinality = len(ep_E1)*len(ep_E1)
-        self.b_size = len(ep_E1) + len(ep_E2)
+    def __init__(self, b_name, ep_E1, ep_E2): 
+        if(self.b_cardinality) != 0: 
+            self.b_name = b_name
+            self.ib1 = ep_E1
+            self.ib2 = ep_E2
+            self.b_cardinality = len(ep_E1)*len(ep_E2)
+            self.b_size = len(ep_E1) + len(ep_E2)
+        
+            pass
+
+    def as_dict(self):
+        return {'block_name': self.b_name, 'int_b1': self.ib1, 'int_b1_size': len(self.ib1), 'int_b2': self.ib2, 'int_b2_size': len(self.ib2), 'b_cardin': self.b_cardinality, 'b_size': self.b_size}
