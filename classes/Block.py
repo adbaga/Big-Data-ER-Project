815c47df9555bb8dba575b8cf1ed56ce36b4085c
adbaga <bagaskaraiqbal7a@gmail.com>
2022-02-12 14:00:40 +0200

    several todo plus additional attributes on the block class

--- classes/Block.py
+++ classes/Block.py
@@ -3,3 +3,5 @@ class Block:
         self.b_name = b_name
         self.ib1 = ep_E1
         self.ib2 = ep_E2
+        self.b_cardinality = len(ep_E1)*len(ep_E1)
+        self.b_size = len(ep_E1) + len(ep_E2)
