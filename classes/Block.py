49af1559179b02d93244d5a079cc277f93f29a97
adbaga <bagaskaraiqbal7a@gmail.com>
2022-02-13 11:13:33 +0200

    fix error in the class block

--- classes/Block.py
+++ classes/Block.py
@@ -1,13 +1,12 @@
 class Block:
-    def __init__(self, b_name, ep_E1, ep_E2): 
-        if(self.b_cardinality) != 0: 
-            self.b_name = b_name
-            self.ib1 = ep_E1
-            self.ib2 = ep_E2
-            self.b_cardinality = len(ep_E1)*len(ep_E2)
-            self.b_size = len(ep_E1) + len(ep_E2)
+    def __init__(self, name, ep_E1, ep_E2): 
         
-            pass
+        self.b_name = name
+        self.ib1 = ep_E1
+        self.ib2 = ep_E2
+        self.b_cardinality = len(ep_E1)*len(ep_E2)
+        self.b_size = len(ep_E1) + len(ep_E2)
+   
 
     def as_dict(self):
         return {'block_name': self.b_name, 'int_b1': self.ib1, 'int_b1_size': len(self.ib1), 'int_b2': self.ib2, 'int_b2_size': len(self.ib2), 'b_cardin': self.b_cardinality, 'b_size': self.b_size}
